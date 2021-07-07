d00f81f1458f762713a447d78c5a5d44910b6e88
AC12344 <annec.poulsen@gmail.com>
2021-07-07 12:52:17 +0200

    finished a very easy version of DL using 5L CNN

--- dpl_w_ml.py
+++ dpl_w_ml.py
@@ -2,7 +2,8 @@ import numpy as np
 import tensorflow as tf 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-import mlflow as ml 
+import mlflow as ml
+from tensorflow import keras 
 
 fashion_data = tf.keras.datasets.fashion_mnist
 
@@ -36,4 +37,38 @@ for i in range(25):
 plt.show()
 
 
+## actual model, very simple one, 5 layers, flattern, 4 dense, 128,64,32,10
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(128,activation='relu'),
+    tf.keras.layers.Dense(64, activation='elu'),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(10)
+    ])
+
+##compile stage, loss function = , optimser=adam, metrics = 
+
+model.compile(optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+        )
+
+## feed the model
+
+model.fit(train_img, train_labels, epochs=20)
+
+#evaluate it
+test_loss, test_Acc = model.evaluate(test_img, test_labels, verbose=2)
+print('\nTest accuracy:', test_Acc)
+
+#Make predictions
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+predict = probability_model.predict(test_img)
+# print all the scores for the first test images
+print(predict[0])
+#print the one it is most confident about
+print('\nWe believe it is: %s, and the right answer is %s.'% 
+        (np.argmax(predict[0]), test_labels[0]))
 
